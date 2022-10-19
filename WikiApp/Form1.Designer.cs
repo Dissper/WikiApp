b99fe8ac088fc015cfd2fb1346634d70b9a00154
Dylan <dyleduardo07@hotmail.com>
2022-10-18 21:30:46 -0600

    Agregar archivos de proyecto.

---
+++ WikiApp/Form1.Designer.cs
@@ -0,0 +1,106 @@
+﻿namespace WikiApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(22, 42);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(365, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.Menu;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Location = new System.Drawing.Point(414, 42);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Buscar";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(19, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(310, 22);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Buscador de Articulos de Wikipedia";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.BackColor = System.Drawing.Color.LightBlue;
+            this.richTextBox1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richTextBox1.Location = new System.Drawing.Point(12, 71);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(865, 520);
+            this.richTextBox1.TabIndex = 5;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightBlue;
+            this.ClientSize = new System.Drawing.Size(889, 603);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Opacity = 0.99D;
+            this.Text = "WikiApp";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ WikiApp/Services/GetSearch.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+
+namespace WikiApp.Services
+{
+    public class GetSearch
+    {
+
+
+
+        /*public void Search(TextBox textbox, Label label)
+        {
+            WebClient client = new WebClient();
+            var searchString = client.DownloadString("https://en.wikipedia.org/w/api.php?action=opensearch&search=" + textbox.Text );
+            var resp = Newtonsoft.Json.JsonConvert.DeserializeObject(searchString);
+            string[] respString = resp.ToString().Split('[');
+            var i = respString[3];
+            label.Text = i;
+        }
+        */
+        public string SearchArticle(string articulo)
+        {
+            WebClient wc = new WebClient();
+            string archivo = wc.DownloadString("https://es.wikipedia.org/w/api.php?format=xml&action=query&prop=extracts&titles=" + articulo + "&redirects=true");
+
+            XmlDocument xml = new XmlDocument();
+            xml.LoadXml(archivo);
+
+            XmlNode nodo = xml.GetElementsByTagName("extract")[0];
+            try
+            {
+                string texto = nodo.InnerText;
+                Regex rx = new Regex("\\<[^\\>]*\\>");
+                texto = rx.Replace(texto, "");
+
+                texto = texto.Replace("Ã", "í");
+                texto = texto.Replace("í³", "ó");
+                texto = texto.Replace("í©", "é");
+                texto = texto.Replace("í±", "ñ");
+                texto = texto.Replace("í¡", "á");
+                texto = texto.Replace("íº", "ú");
+                texto = texto.Replace("â€”", "—");
+                texto = texto.Replace("Â", "");
+                texto = texto.Replace("â€‹", "");
+
+                return texto;
+            }
+            catch (Exception ex) { return "El artículo no existe"; }
+        }
+
+    }
+}
